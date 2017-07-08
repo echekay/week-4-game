e1c0de38fe4acd46769fe96e93684dcc3d8dd700
Hongsyn Keath <Hongsyn Keath>
2017-07-08 10:34:24 -0400

    HTML complete, javascript start of logic.

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+   $(document).ready(function() {
+
+
+
+
+// ----------------------------------------
+   	var winsCounter = 0;
+   	var lossesCounter = 0;
+// ----------------------------------------
+	var targetScore = Math.floor(Math.random() * (120 - 19) + 19);
+
+	console.log("hello");
+
+// 	function getRandomArbitrary(min, max) {
+//   return Math.random() * (max - min) + min;
+// }
+
+
+
+   }
