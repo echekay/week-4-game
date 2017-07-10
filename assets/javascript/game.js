2070b1fbd313455d25391ec332a9aa9550415626
Hongsyn Keath <Hongsyn Keath>
2017-07-09 23:52:17 -0400

    Updated files, javascript and the games logic is incomplete. Need to add restart function and fix counter issue.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,54 +2,85 @@ $(document).ready(function() {
 
 
 
-// ----------------------------------------
+    // ----------------------------------------
     // Displays users total wins amount.
-   	var winsCounter = 0;
+    var winsCounter = 0;
     // Displays users total losses.
-   	var lossesCounter = 0;
+    var lossesCounter = 0;
     // Users score counter.
-   	var userCounter = 0;
+    var userCounter = 0;
     // Generate random value between 19 and 120.
     var randomScore = Math.floor(Math.random() * 101) + 19;
-
-
-    $("#targetScore").html(randomScore);
-
-    for (i = 0; i < 4; i++) {
-      // Generate random value for crystal image between 1 and 12.
-      var randomCrystalValue = Math.floor(Math.random() * 12) + 1;
-      $(".crystal").attr("data-crystalvalue", [i]);
-
-    }
-  
+    // Generate random value for crystals between 1 and 12.
+    var num1 = Math.floor(Math.random() * 12) + 1;
+    var num2 = Math.floor(Math.random() * 12) + 1;
+    var num3 = Math.floor(Math.random() * 12) + 1;
+    var num4 = Math.floor(Math.random() * 12) + 1;
+    $("#userWins").text(winsCounter);
+    $("#userLosses").text(lossesCounter);
 
 
 
 
-// ------------------------------------------------------------
-	// if (parseInt(userCounter) === targetScore) {
-      // ; Might not need the parseInt if im not grabbing a string value to convert it to a number value.
-    // }
+    $(document).on("click", function() {
 
-    // else if (userCounter >= targetScore) {
-      // alert("You lose!!");
-    // }
+        $("#targetScore").html(randomScore);
+        $("#userCounter").html(userCounter);
+
+        $("#firstCrystal").on("click", function() {
+
+            userCounter = userCounter + num1;
+            console.log(userCounter);
+            $("#userCounter").html(userCounter);
+            if (userCounter === randomScore) {
+                winsCounter++;
+            } else {
+                lossesCounter++;
+            }
+        });
+
+        $("#secondCrystal").on("click", function() {
+
+            userCounter = userCounter + num2;
+            console.log(userCounter);
+            $("#userCounter").html(userCounter);
+            if (userCounter === randomScore) {
+                winsCounter++;
+            } else {
+                lossesCounter++;
+            }
+        });
+
+        $("#thirdCrystal").on("click", function() {
+
+            userCounter = userCounter + num3;
+            console.log(userCounter);
+            $("#userCounter").html(userCounter);
+            if (userCounter === randomScore) {
+                winsCounter++;
+            } else {
+                lossesCounter++;
+            }
+        });
+
+        $("#fourthCrystal").on("click", function() {
+
+            userCounter = userCounter + num4;
+            console.log(userCounter);
+            $("#userCounter").html(userCounter);
+            if (userCounter === randomScore) {
+                winsCounter++;
+            } else {
+                lossesCounter++;
+            }
+        });
+    })
+
+
+
+
+
+
 
 
 })
-
-// So as webpage gets loaded:
-// Generate random value for targetscore.
-// Assign the variable from the random value to the div where it will be displayed.
-// Generate random values for each crystal image
-// Assign each random value to the image.
-// Document when the user clicks on the image.
-// Add the value of each image click to the userCounter.
-// If the value equals the value of the targetscore, display win message and update wins counter.
-// If the value exceeds the value of targetscore, display loss meassage and update counter.
-// Reset all values and start game over.
-
-
-
-
-   
